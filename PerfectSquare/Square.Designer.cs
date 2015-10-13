d3c4591889d91aa319777049cf8e0716a9a4df58
PerryMartin <perry.martin@ocsbstudent.ca>
2015-10-13 13:23:10 -0400

    Perfect Square
    pepe

---
+++ PerfectSquare/Square.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PerfectSquare
+{
+    partial class frmSquare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lstOfNumbers = new System.Windows.Forms.ListBox();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(112, 87);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lstOfNumbers
+            // 
+            this.lstOfNumbers.FormattingEnabled = true;
+            this.lstOfNumbers.Location = new System.Drawing.Point(13, 176);
+            this.lstOfNumbers.Name = "lstOfNumbers";
+            this.lstOfNumbers.Size = new System.Drawing.Size(286, 264);
+            this.lstOfNumbers.TabIndex = 1;
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(99, 37);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(120, 20);
+            this.nudNumber.TabIndex = 2;
+            // 
+            // frmSquare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 456);
+            this.Controls.Add(this.nudNumber);
+            this.Controls.Add(this.lstOfNumbers);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmSquare";
+            this.Text = "Perfect Square";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lstOfNumbers;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+    }
+}
+
---
+++ PerfectSquare/Square.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Created by: Perry Martin
+ * Created on: 13-10-2015
+ * Created for: ICS3U
+ * Daily Assignment – unit#3-09
+ * This program displays a perfect square
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquare
+{
+    public partial class frmSquare : Form
+    {
+        public frmSquare()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int value = 1;
+            int endingValue;
+            int squareRootAsInteger;
+             int numberOfPerfectSquares = 0;
+              double squareRootAsDouble;
+            this.lstOfNumbers.Items.Clear();
+            
+
+            endingValue = Convert.ToInt32(this.nudNumber.Text);
+
+
+            while (value <= endingValue)
+            {
+                squareRootAsDouble = Math.Sqrt(value);
+                squareRootAsInteger = Convert.ToInt32(squareRootAsDouble);
+
+
+                if (squareRootAsInteger == squareRootAsDouble)
+                {
+                    numberOfPerfectSquares++;
+                    this.lstOfNumbers.Items.Add(value);
+                }
+
+
+                value++;
+            }
+         }
+    }
+}
+
